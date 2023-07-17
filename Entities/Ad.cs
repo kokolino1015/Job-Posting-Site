24884ccf1937e0f2bd26ee727129d643447acb1f
Egor Zhelyaskov <97658518+Ne9or@users.noreply.github.com>
2023-07-17 09:37:27 +0300

    Add files via upload

---
+++ Entities/Ad.cs
@@ -0,0 +1,13 @@
+﻿using Job_Posting_Site.Data.Entities.Account;
+
+namespace Job_Posting_Site.Data.Entities
+{
+    public class Ad
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<ApplicationUser> Candidates { get; set; }
+        public ApplicationUser Owner { get; set; }
+    }
+}
---
+++ Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Job_Posting_Site.Data.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string ?Name { get; set; }
+    }
+}
