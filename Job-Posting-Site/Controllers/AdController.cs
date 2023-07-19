51c294ebd724bcbd7d9b7c4305560cf7e45fe3de
Egor Zhelyaskov <97658518+Ne9or@users.noreply.github.com>
2023-07-19 17:41:59 +0300

    Ad and Home controllers update

--- Job-Posting-Site/Controllers/AdController.cs
+++ Job-Posting-Site/Controllers/AdController.cs
@@ -9,7 +9,7 @@ namespace Job_Posting_Site.Controllers
 {
     public class AdController : Controller
     {
-        private readonly AdService adService;
+        private readonly AdService adService ;
         private readonly CommonService commonService;
         public AdController(AdService adService, CommonService commonService)
         {
@@ -21,7 +21,6 @@ namespace Job_Posting_Site.Controllers
         public IActionResult Create()
         {
             AdFormModel model = new AdFormModel();
-            ViewBag.Owner = commonService.FindUser(User);
             ViewBag.Categories = adService.GetCategories();
             return View(model);
         }
@@ -29,6 +28,7 @@ namespace Job_Posting_Site.Controllers
         [HttpPost]
         public IActionResult Create(AdFormModel model)
         {
+            model.Owner = commonService.FindUser(User);
             adService.Create(model);
             return RedirectToAction("Index", "Home");
         }
@@ -36,11 +36,11 @@ namespace Job_Posting_Site.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            if (!adService.CheckIfOwner(id, commonService.FindUser(User)))
+            if(!adService.CheckIfOwner(id, commonService.FindUser(User)))
             {
                 return Unauthorized();
             }
-            this.ViewBag.Owner = commonService.FindUser(User);
+            ViewBag.Categories = adService.GetCategories();
             return View(adService.GetAdById(id));
         }
         [Authorize]
@@ -51,10 +51,56 @@ namespace Job_Posting_Site.Controllers
             return RedirectToAction("Index", "Home");
         }
         [HttpGet]
-        public IActionResult Details(int id)
+        public IActionResult Details(int id) 
         {
-            ViewBag.Role = commonService.FindUser(User).Role.Name;
+            var user = commonService.FindUser(User);
+            var role = commonService.FindRole(User);
+            if(role != null) {
+                ViewBag.Role = commonService.FindRole(User).Name;
+            }
+            ViewBag.Show = false;
+            var model = adService.GetAdById(id);
+            if (adService.checkifUserInCandidateList(model, user))
+            {
+                ViewBag.Show = true; 
+            }                   
+            ViewBag.Category = adService.GetCategoryById(model.Category).Name;
+            return View(model);
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (!adService.CheckIfOwner(id, commonService.FindUser(User)))
+            {
+                return Unauthorized();
+            }
             return View(adService.GetAdById(id));
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult Delete(AdFormModel model)
+        {
+            if (!adService.CheckIfOwner(model.Id, commonService.FindUser(User)))
+            {
+                return Unauthorized();
+            }
+            adService.Delete(model.Id);
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Apply(int  id)
+        {
+            adService.ApplyUser(commonService.FindUser(User), adService.GetAdById(id));
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Unapply(int id)
+        {
+            adService.UnapplyUser(commonService.FindUser(User), adService.GetAdById(id));
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Job-Posting-Site/Controllers/HomeController.cs
+++ Job-Posting-Site/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Job_Posting_Site.Models;
+﻿using Job_Posting_Site.Data.Entities;
+using Job_Posting_Site.Models;
+using Job_Posting_Site.Models.AdViewModel;
+using Job_Posting_Site.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +10,26 @@ namespace Job_Posting_Site.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AdService adService;
+        private readonly CommonService commonService;
+        public HomeController(ILogger<HomeController> logger, AdService adService, CommonService commonService)
         {
             _logger = logger;
+            this.commonService = commonService;
+            this.adService = adService;
         }
 
         public IActionResult Index()
         {
+            List<AdFormModel> ads = adService.GetFirst10Ads();
+            var role = commonService.FindRole(User);
+            if (role != null)
+            {
+                ViewBag.Role = commonService.FindRole(User).Name;
+            }
+            ViewBag.Ads = ads;
+            ViewBag.Owner = commonService.FindUser(User);
+            ViewBag.Categories = adService.GetCategories();
             return View();
         }
 
