9dd7fb783b668dce7c02b55e9ad9ce321758aaa1
Egor Zhelyaskov <97658518+Ne9or@users.noreply.github.com>
2023-07-19 14:27:38 +0300

    Views and AdFormModel

---
+++ Job-Posting-Site/AdViewModel/AdFormModel.cs
@@ -0,0 +1,19 @@
+﻿using Job_Posting_Site.Data.Entities;
+using Job_Posting_Site.Data.Entities.Account;
+using System.ComponentModel.DataAnnotations;
+
+namespace Job_Posting_Site.Models.AdViewModel
+{
+    public class AdFormModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Category { get; set; }
+        [Required]
+        public ApplicationUser Owner { get; set; }
+        public List<ApplicationUser> Candidates { get; set; } = new List<ApplicationUser>();
+    }
+}
